d37669d100acf6e769deb3e580221ce7566801d8
voidz777 <voidz.evo3d@gmail.com>
2017-01-06 23:04:21 +0900

    apns

--- overrides.mk
+++ overrides.mk
@@ -1,3 +1,3 @@
 # Get the good APNs, not the ones in 'goldfish'.
 PRODUCT_COPY_FILES += \
-	device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+	$(DEVICE_FOLDER)/prebuilt/etc/apns-full-conf.xml:system/etc/apns-conf.xml
